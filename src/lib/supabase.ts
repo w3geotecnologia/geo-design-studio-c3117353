0a8a405e47c91897303e91eebc35e00ae3964428
w3geotecnologia <w3.geotecnologia@gmail.com>
2026-05-30 18:18:03 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
-  import.meta.env.VITE_SUPABASE_URL ?? "https://adm.negociotopogafico.com.br";
+  import.meta.env.VITE_SUPABASE_URL ?? "https://adm.negociotopografico.com.br";
 const supabaseAnonKey =
   import.meta.env.VITE_SUPABASE_ANON_KEY ??
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc3NjYxODU0MCwiZXhwIjo0OTMyMjkyMTQwLCJyb2xlIjoiYW5vbiJ9.IvOY9oT8txAdekYA8j3m0dP8tQwKQtmRWJ_wRTsSG1o";
